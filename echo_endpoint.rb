0ab47117bc98746b4fa16cdfc8bf8d72a2a3080e
Brian Quinn <briandquinn@gmail.com>
2014-04-09 11:42:18 +0100

    Add new /ready action that checks for any object, and insists its 'status' be 'ready'

--- echo_endpoint.rb
+++ echo_endpoint.rb
@@ -16,4 +16,28 @@ class EchoEndpoint < EndpointBase::Sinatra::Base
   post '/fail' do
     result 500, 'Echo Fail Response'
   end
+
+  post '/ready' do
+    code = 500
+    summary = 'Payload was not very interesting.'
+
+    @payload.each do |key, value|
+      next if %w{request_id parameters}.include? key
+
+      if value.is_a? Hash
+        if value['status'] == 'ready'
+          code = 200
+          summary = "The '#{key}' object is ready!"
+        else
+          code = 500
+          summary = "The '#{key}' object is invalid for this action, it's 'status' MUST be 'ready', but it WAS '#{value['status']}'."
+        end
+      else
+        code = 500
+        summary = "The '#{key}' must be an object, and it was a #{value.class}."
+      end
+    end
+
+    result code, summary
+  end
 end
